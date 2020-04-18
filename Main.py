eea5eddfc28c35a3c42a7aaf94a63efb622bfa44
Arceus1ooo <56331076+Arceus1ooo@users.noreply.github.com>
2020-04-18 17:28:31 -0400

    Added room switching

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import pygame
-from PlayerController import Player
 from GlobalVariables import *
+from PlayerController import Player
+from Triggers import RoomSwitch
 
 pygame.init()
 
@@ -9,15 +10,24 @@ timer = pygame.time.Clock()
 window = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Dungeon Knight")
 
-background = pygame.image.load('background.jpg')
-
 player = Player(300, 300, 64, 64)
+rightSwitch = RoomSwitch(screenWidth - 5, screenHeight / 2, 5, 40, 'right')
 
 
 def redrawWindow():
+    background = pygame.image.load(map[mapX][mapY])
     window.blit(background, (0, 0))
     player.redraw(window)
-    pygame.display.update()
+    rightSwitch.redraw(window)
+    pygame.display.update()  # This should always be last
+
+
+def detectCollision(obj1, obj2):
+    if (obj1.x + obj1.width) > obj2.x and obj1.x < (obj2.x + obj2.width):
+        if (obj1.y + obj1.height) > obj2.y and obj1.y < (obj2.y + obj2.height):
+            return True
+    else:
+        return False
 
 
 # MAIN GAME LOOP
@@ -29,7 +39,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    collision = detectCollision(player, rightSwitch)
+    if collision and mapY < mapYMax:
+        mapY = rightSwitch.trigger(mapX, mapY)
+        player.x = 0
     player.movement()
+    # print("Map X: " + str(mapX) + ", Map Y: " + str(mapY))
     redrawWindow()
 
 pygame.quit()
