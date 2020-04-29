201ce6553a8f6037827b5a0a63cd744401fccaed
Arceus1ooo <56331076+Arceus1ooo@users.noreply.github.com>
2020-04-29 16:44:54 -0400

    removed white background from javelin sprite, but it looks weird

--- PlayerController.py
+++ PlayerController.py
@@ -82,7 +82,7 @@ class Player:
 class Javelin:
     def __init__(self, x, y, mousePos):
         self.speed = 8
-        self.image = pygame.image.load('javelin.jpg')
+        self.image = pygame.image.load('javelin.png')
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
