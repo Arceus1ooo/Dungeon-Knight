a7512a949741116a91f4e3569e826672a87dbdf1
pdamiano-11 <63809581+pdamiano-11@users.noreply.github.com>
2020-04-30 00:53:08 -0400

    Add files via upload

--- Puzzle.py
+++ Puzzle.py
@@ -1,5 +1,6 @@
 import pygame
 from GlobalVariables import *
+import random
 
 not_press = 1
 
@@ -12,16 +13,16 @@ class puzzle:
         self.rooms = rooms
         self.existence = 0
         self.position = (self.x, self.y)
-        self.color = red
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.alpha = 255
         self.x_button = x_button
         self.y_button = y_button
+        self.not_press = 1
 
     def wallCollision(self, sprite):
-        global not_press
         if (self.x + self.width) > sprite.x and self.x < (sprite.x + sprite.width):
             if (self.y + self.height) > sprite.y and self.y < (sprite.y + sprite.height):
-                if self.alpha == 255 and not_press == 1:
+                if self.alpha == 255 and self.not_press == 1:
                     if sprite.direction == 'up':
                         sprite.y += sprite.speed
                     elif sprite.direction == 'down':
@@ -34,40 +35,40 @@ class puzzle:
             return False
 
     def redraw(self, win, roomNum):
-        global not_press
         self.existence = 0
         for room in self.rooms:
             if room == roomNum:
                 self.existence += 1
-        if self.existence > 0 and not_press == 1:
+        if self.existence > 0 and self.not_press == 1:
             self.alpha = 255
-            s = pygame.Surface((self.width, self.height))
-            s.set_alpha(self.alpha)
-            s.fill(black)
-            win.blit(s, (self.x, self.y))
             b = pygame.Surface((30, 30))
             b.set_alpha(self.alpha)
-            b.fill(black)
+            b.fill(self.color)
             win.blit(b, (self.x_button, self.y_button))
+            s = pygame.Surface((self.width, self.height))
+            s.set_alpha(self.alpha)
+            s.fill(self.color)
+            win.blit(s, (self.x, self.y))
         else:
             self.alpha = 0
             s = pygame.Surface((self.width, self.height))
             s.set_alpha(self.alpha)
-            s.fill(black)
+            s.fill(self.color)
             win.blit(s, (self.x, self.y))
             b = pygame.Surface((30, 30))
             b.set_alpha(self.alpha)
-            b.fill(black)
+            b.fill(self.color)
             win.blit(b, (self.x_button, self.y_button))
 
     def buttonPress(self, sprite):
-        global not_press
-        if (self.x_button + 30) > sprite.x and self.x_button < (sprite.x + sprite.width):
-            if (self.y_button + 30) > sprite.y and self.y_button < (sprite.y + sprite.height):
-                if self.alpha == 255:
-                    not_press = 0
-        else:
-            not_press = 1
+        if sprite.alpha == 255:
+            if (self.x_button + 30) > sprite.x and self.x_button < (sprite.x + sprite.width):
+                if (self.y_button + 30) > sprite.y and self.y_button < (sprite.y + sprite.height):
+                    if self.alpha == 255:
+                        self.not_press = 0
+                        return True
+            else:
+                self.not_press = 1
 
 
    
