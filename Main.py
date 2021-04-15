9f15f7e1404180ffc5bfb6324c9e5aabbd1f6bee
pdamiano-11 <pdamiano@stevens.edu>
2021-04-15 14:56:05 -0400

    fixed whitespace issues and imports and indents

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import pygame
-from GlobalVariables import *
+from GlobalVariables import screenWidth, screenHeight, frameSpeed, mainRoom, room1, room2
+from GlobalVariables import room3, room4, room5, room6, room7, room8, gray, black, mapX, mapY
 from PlayerController import Player, Javelin
 from Triggers import RoomSwitch
 from EnemyController import Enemy
@@ -22,11 +23,14 @@ pointerRect = pointerImg.get_rect()
 player = Player(300, 300, 64, 64)
 javelins = []
 
-rightSwitch = RoomSwitch(screenWidth - 5, (screenHeight / 2) - 75, 5, 100, 'right', [mainRoom, room2, room4])
-leftSwitch = RoomSwitch(0, (screenHeight / 2) - 75, 5, 100, 'left', [mainRoom, room3, room5])
-bottomSwitch = RoomSwitch(screenWidth / 2 - 75, screenHeight - 5, 150, 5, 'down',
-                          [mainRoom, room1, room2, room4, room5])
-topSwitch = RoomSwitch(screenWidth / 2 - 75, 0, 150, 5, 'up', [mainRoom, room7, room6, room4, room8])
+rightSwitch = RoomSwitch(screenWidth - 5, (screenHeight / 2) - 75, 5, 100,
+                         'right', [mainRoom, room2, room4])
+leftSwitch = RoomSwitch(0, (screenHeight / 2) - 75, 5, 100, 'left',
+                        [mainRoom, room3, room5])
+bottomSwitch = RoomSwitch(screenWidth / 2 - 75, screenHeight - 5, 150, 5,
+                          'down', [mainRoom, room1, room2, room4, room5])
+topSwitch = RoomSwitch(screenWidth / 2 - 75, 0, 150, 5, 'up',
+                       [mainRoom, room7, room6, room4, room8])
 roomSwitches = [rightSwitch, leftSwitch, topSwitch, bottomSwitch]
 
 yeti = Enemy(100, 300, 64, 64, 'spr_ape_yeti.png')
@@ -36,18 +40,30 @@ enemies = [yeti]
 topLeft = LWall(0, 0, 365, 250, 0.27, 0.39, 'topLeft')
 topRight = LWall(screenWidth, 0, 365, 250, 0.27, 0.38, 'topRight')
 bottomLeft = LWall(0, screenHeight, 365, 295, 0.27, 0.2, 'bottomLeft')
-bottomRight = LWall(screenWidth, screenHeight, 365, 295, 0.25, 0.2, 'bottomRight')
-leftWall = Barricade(0, topLeft.y + topLeft.height, topLeft.scaledWidth, topLeft.scaledHeight + 40, gray,
+bottomRight = LWall(screenWidth, screenHeight, 365, 295, 0.25, 0.2,
+                    'bottomRight')
+
+leftWall = Barricade(0, topLeft.y + topLeft.height, topLeft.scaledWidth,
+                     topLeft.scaledHeight + 40, gray,
                      [room6, room4, room1, room7, room2, room8])
-topWall = Barricade(topLeft.x + topLeft.width, 0, topLeft.scaledWidth + 135, topLeft.scaledHeight, gray,
-                    [room1, room2, room3, room5])
-rightWall = Barricade(screenWidth - bottomRight.scaledWidth, topRight.y + topRight.height, topRight.scaledWidth,
-                      topRight.scaledHeight + 40, gray, [room6, room1, room7, room8, room5, room3])
-bottomWall = Barricade(topLeft.x + topLeft.width, screenHeight - bottomRight.scaledHeight,
-                       bottomLeft.scaledWidth + 135, topLeft.scaledHeight, gray, [room6, room7, room8, room3])
+
+topWall = Barricade(topLeft.x + topLeft.width, 0, topLeft.scaledWidth + 135,
+                    topLeft.scaledHeight, gray, [room1, room2, room3, room5])
+
+rightWall = Barricade(screenWidth - bottomRight.scaledWidth,
+                      topRight.y + topRight.height, topRight.scaledWidth,
+                      topRight.scaledHeight + 40, gray,
+                      [room6, room1, room7, room8, room5, room3])
+
+bottomWall = Barricade(topLeft.x + topLeft.width,
+                       screenHeight - bottomRight.scaledHeight,
+                       bottomLeft.scaledWidth + 135, topLeft.scaledHeight,
+                       gray, [room6, room7, room8, room3])
+
 barrier11 = Barricade(300, 400, 325, 10, black, [room5])
 barrier12 = Barricade(335, 410, 10, 215, black, [room5])
-walls = [topLeft, topRight, bottomLeft, bottomRight, leftWall, topWall, rightWall, bottomWall, barrier11, barrier12]
+walls = [topLeft, topRight, bottomLeft, bottomRight, leftWall, topWall,
+         rightWall, bottomWall, barrier11, barrier12]
 barriers = [leftWall, topWall, rightWall, bottomWall, barrier11, barrier12]
 
 block51 = moveObj(150, 250, 25, 25, [room5])
@@ -72,8 +88,8 @@ puzzles = [puzzle51, puzzle52, puzzle53, puzzle54, puzzle41, puzzle42, puzzle2]
 
 def redrawWindow():
     room = map[mapX][mapY]
-    for e in enemies:
-        if e.health > 0:
+    for enemy in enemies:
+        if enemy.health > 0:
             window.blit(pygame.image.load(map[mapX][mapY]), (0, 0))
     enemy_list.draw(window)
     if mapX == 1 and mapY == 1:
@@ -81,18 +97,18 @@ def redrawWindow():
         enemy_list.draw(window)
     else:
         enemy_list.remove(yeti)
-    for e in enemies:
-        e.moveTowardsPlayer(player)
-        if e.health == 0:
-            e.kill()
+    for enemy in enemies:
+        enemy.moveTowardsPlayer(player)
+        if enemy.health == 0:
+            enemy.kill()
             window.blit(pygame.image.load(map[mapX][mapY]), (0, 0))
             player.redraw(window)
     for javelin in javelins:
         javelin.redraw(window)
     for switch in roomSwitches:
         switch.redraw(window, room)
-    for puzzle in puzzles:
-        puzzle.redraw(window, room)
+    for item in puzzles:
+        item.redraw(window, room)
     for block in movingBlocks:
         block.redraw(window, room)
     for wall in walls:
@@ -111,10 +127,11 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN and len(javelins) == 0:
             javelins.append(
-                Javelin(player.x + player.width / 2, player.y + player.height / 2, pygame.mouse.get_pos()))
+                Javelin(player.x + player.width / 2,
+                        player.y + player.height / 2, pygame.mouse.get_pos()))
     mouse = pygame.mouse.get_pos()
     pointerRect.topleft = (mouse[0] - 5, mouse[1] - 5)
-    
+
     # Player collisions with room switches
     for switch in roomSwitches:
         switch.collision = switch.detectCollision(player)
@@ -127,19 +144,19 @@ while running:
                 player.y = switch.postTrigger(player.width, player.height)
 
     for javelin in javelins:
-        for e in enemy_list:
-            if javelin.detectCollision(e):
-                e.health -= player.attack
+        for enemy in enemy_list:
+            if javelin.detectCollision(enemy):
+                enemy.health -= player.attack
                 javelin.visible = False
                 # print(e.health)
         for w in walls:
             if w.detectRectCollision(javelin):
                 javelin.visible = False
 
-    for e in enemy_list:
-        if (e.rect.x + e.width) > player.x and e.rect.x < (player.x + player.width):
-            if (e.rect.y + e.height) > player.y and e.rect.y < (player.y + player.height):
-                player.health -= e.attack
+    for enemy in enemy_list:
+        if((enemy.rect.x + enemy.width) > player.x and enemy.rect.x < (player.x + player.width)):
+            if ((enemy.rect.y + enemy.height) > player.yeti and enemy.rect.y < (player.y + player.height)):
+                player.health -= enemy.attack
                 # print(player.health)
 
     for block in movingBlocks:
@@ -151,7 +168,7 @@ while running:
     for bar in barriers:
         for block in movingBlocks:
             block.collisionBlock(bar)
-    
+
     for p in puzzles:
         p.buttonPress(player)
         for block in movingBlocks:
